30e07ba19782104d7ca80677b0ac19e9c4816660
김오영 <kdhdud15@naver.com>
2019-04-01 20:13:28 +0900

    Add xml

---
+++ app/src/main/java/com/example/a502/smarthome/RoomActivity.java
@@ -0,0 +1,12 @@
+package com.example.a502.smarthome;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RoomActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+    }
+}
