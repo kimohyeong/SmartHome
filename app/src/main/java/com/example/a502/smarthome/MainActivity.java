639d88a6f2a08d36b830285b21e238609e2d65d6
김오영 <kdhdud15@naver.com>
2019-04-17 20:39:37 +0900

    Goto ParticleApp

--- app/src/main/java/com/example/a502/smarthome/MainActivity.java
+++ app/src/main/java/com/example/a502/smarthome/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.a502.smarthome;
 
 import android.app.Dialog;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
@@ -311,5 +315,51 @@ public class MainActivity extends AppCompatActivity {
             editor.putString("Name5",name5);
         }
         editor.commit();
+    }
+
+
+    public void plusDevice(View v)
+    {
+
+        //Particle앱 존재 검사
+        boolean isExist = false;
+
+        PackageManager pkgMgr = getPackageManager();
+        List<ResolveInfo> mApps;
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        mApps = pkgMgr.queryIntentActivities(mainIntent, 0);
+
+        try {
+            for (int i = 0; i < mApps.size(); i++) {
+                if(mApps.get(i).activityInfo.packageName.startsWith("io.particle.android.app")){
+                    isExist = true;
+                    break;
+                }
+            }
+        }
+        catch (Exception e) {
+            isExist = false;
+        }
+
+        //존재하면 앱 실행
+        if(isExist)
+        {
+            ComponentName compName=new ComponentName("io.particle.android.app","io.particle.android.sdk.ui.DeviceListActivity");
+            Intent intent =new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.setComponent(compName);
+
+            // Intent intent = getPackageManager().getLaunchIntentForPackage("io.particle.android.app");
+            // intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+        else
+        {
+            String url = "market://details?id=" + "io.particle.android.app";
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(i);
+        }
+
     }
 }
